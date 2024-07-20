0f940be428f834bc73729aab0396d8cc6528e11a
Dbinggo <djy1349678887@163.com>
2024-07-21 02:37:25 +0800

    feat: 不使用 const 文件 纯zlog文件使用

--- common/zlog/zlog.go
+++ common/zlog/zlog.go
@@ -8,6 +8,18 @@ import (
 	"runtime"
 )
 
+const (
+	logger_formate_json = "json"
+	logger_key_log      = "debug"
+	logger_key_caller   = "caller"
+
+	loggerKey   = "logger"
+	loggerExKey = "loggerEx"
+
+	// 分隔符
+	formatSeparator = "%v\t"
+)
+
 type ZlogConfig struct {
 	Format     string
 	Debug      bool
