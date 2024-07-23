beb8ae9ffd7c9a18dfa81285b1bfd200e4101e56
YJDDB <djy1349678887@163.com>
2024-07-23 17:48:04 +0800

    feat(foo): 设置example

--- app/api/foo.go
+++ app/api/foo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Ai-HireSphere/utils"
 	"flag"
 	"fmt"
 
@@ -12,7 +13,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/user.yaml", "the config file")
+var configFile = flag.String("f", utils.GetRootPath("/app/api/etc/foo.yaml"), "the config file")
 
 func main() {
 	flag.Parse()
--- app/api/internal/handler/routes.go
+++ app/api/internal/handler/routes.go
@@ -4,8 +4,6 @@ package handler
 import (
 	"net/http"
 
-	login "Ai-HireSphere/app/api/internal/handler/login"
-	user "Ai-HireSphere/app/api/internal/handler/user"
 	"Ai-HireSphere/app/api/internal/svc"
 
 	"github.com/zeromicro/go-zero/rest"
@@ -15,28 +13,10 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		[]rest.Route{
 			{
-				Method:  http.MethodPost,
-				Path:    "/user/login",
-				Handler: login.LoginHandler(serverCtx),
+				Method:  http.MethodGet,
+				Path:    "/ping",
+				Handler: pingHandler(serverCtx),
 			},
 		},
-		rest.WithPrefix("/v1"),
-	)
-
-	server.AddRoutes(
-		[]rest.Route{
-			{
-				Method:  http.MethodPost,
-				Path:    "/user/info",
-				Handler: user.GetUserInfoHandler(serverCtx),
-			},
-			{
-				Method:  http.MethodPost,
-				Path:    "/user/info/update",
-				Handler: user.UpdateUserInfoHandler(serverCtx),
-			},
-		},
-		rest.WithJwt(serverCtx.Config.Auth.AccessSecret),
-		rest.WithPrefix("/v1"),
 	)
 }
