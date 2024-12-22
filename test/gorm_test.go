4f084dd9e4ce1a3570e01e08e7fe703e7f10212c
dbinggodai <djy1349678887@163.com>
2024-12-22 15:08:11 +0800

    feat(init): 暂存提交

--- test/gorm_test.go
+++ test/gorm_test.go
@@ -1,18 +1,1 @@
 package test
-
-import (
-	"Ai-HireSphere/common/model"
-	"gorm.io/gorm"
-	"testing"
-)
-
-func TestGorm(t *testing.T) {
-
-	db := gorm.DB{}
-
-	db.Transaction(func(tx *gorm.DB) error {
-		ret := &model.Example{}
-		model.NewService().GetOne(tx, "阿爸阿爸", ret)
-		return nil
-	})
-}
