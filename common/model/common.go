bad62078ebf13e948cfad0fd42e0e1ed469b0221
dbinggodai <djy1349678887@163.com>
2025-01-24 11:08:13 +0800

    feat(common): 数据库接口适配器使用指针模式

--- common/model/common.go
+++ common/model/common.go
@@ -18,51 +18,55 @@ type ICommonModel interface {
 
 // 数据库接口适配器，提供最基本CURD能力
 type IDBAdapter[T ICommonModel] interface {
-	GetOne(db *gorm.DB, where ...interface{}) (T, error)
-	GetMulti(db *gorm.DB, where ...interface{}) ([]T, error)
-	Create(db *gorm.DB, data T) (T, error)
-	UpdateOne(db *gorm.DB, data T, selects ...string) (T, error)
+	GetOne(db *gorm.DB, where ...interface{}) (*T, error)
+	GetMulti(db *gorm.DB, where ...interface{}) (*[]*T, error)
+	Save(db *gorm.DB, data *T) (*T, error)
+	UpdateOne(db *gorm.DB, data *T, selects ...string) (*T, error)
 	Delete(db *gorm.DB, ids ...[]int) error
 }
 
 type CommonAdapter[T ICommonModel] struct{}
 
-func (c *CommonAdapter[T]) GetOne(db *gorm.DB, where ...interface{}) (T, error) {
+func (c *CommonAdapter[T]) GetOne(db *gorm.DB, where ...interface{}) (*T, error) {
 	return _getOne[T](db, where)
 }
-func (c *CommonAdapter[T]) GetMulti(db *gorm.DB, where ...interface{}) ([]T, error) {
+
+func (c *CommonAdapter[T]) GetMulti(db *gorm.DB, where ...interface{}) (*[]*T, error) {
 	return _getMulti[T](db, where)
 }
-func (c *CommonAdapter[T]) Create(db *gorm.DB, data T) (T, error) {
-	return _create[T](db, data)
+
+func (c *CommonAdapter[T]) Save(db *gorm.DB, data *T) (*T, error) {
+	return _save[T](db, data)
 }
-func (c *CommonAdapter[T]) UpdateOne(db *gorm.DB, data T, selects ...string) (T, error) {
+
+func (c *CommonAdapter[T]) UpdateOne(db *gorm.DB, data *T, selects ...string) (*T, error) {
 	return _updateOne[T](db, data, selects...)
 }
+
 func (c *CommonAdapter[T]) Delete(db *gorm.DB, ids ...[]int) error {
 	return _delete[T](db, ids...)
 }
 
-func _getOne[T ICommonModel](tx *gorm.DB, where ...interface{}) (T, error) {
+func _getOne[T ICommonModel](tx *gorm.DB, where ...interface{}) (*T, error) {
 	var ret T
 	err := tx.Model(&ret).Where(where).First(&ret).Error
-	return ret, err
+	return &ret, err
 }
 
-func _getMulti[T ICommonModel](tx *gorm.DB, where ...interface{}) ([]T, error) {
-	var ret []T
+func _getMulti[T ICommonModel](tx *gorm.DB, where ...interface{}) (*[]*T, error) {
+	var ret []*T
 	var name T
 	err := tx.Model(&name).Where(where).Find(&ret).Error
-	return ret, err
+	return &ret, err
 }
 
-func _create[T ICommonModel](tx *gorm.DB, data T) (T, error) {
+func _save[T ICommonModel](tx *gorm.DB, data *T) (*T, error) {
 	err := tx.Model(&data).Create(&data).Error
 	return data, err
 }
 
-func _updateOne[T ICommonModel](tx *gorm.DB, data T, selects ...string) (T, error) {
-	if selects == nil {
+func _updateOne[T ICommonModel](tx *gorm.DB, data *T, selects ...string) (*T, error) {
+	if selects == nil || len(selects) == 0 || selects[0] == "" {
 		selects = []string{"*"}
 	}
 	err := tx.Model(&data).Select(selects).Updates(&data).Error
